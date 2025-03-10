b66c3f967573301a2e565370308d0b295364def9
Anton Kefilev <kefilev@yahoo.com>
2025-03-10 10:05:13 +0200

    Add subscribers table and controller

---
+++ app/Http/Controllers/SubscriberController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Subscriber;
+use Illuminate\Http\Request;
+
+class SubscriberController extends Controller
+{
+    public function subscribe(Request $request) {
+        try {
+            //Validate params
+            $validated = $request->validate([
+                'email' => 'bail|required||email:rfc,dns|unique:subscribers',
+                'percent' => 'bail|required|numeric',
+                'period' => 'bail|required|in:1,6,24'
+            ]);
+
+            //Save to DB
+            $subscriber = new Subscriber();
+            $subscriber->email = $request->query('email');
+            $subscriber->percent = $request->query('percent');
+            $subscriber->period = $request->query('period');
+            $saved = $subscriber->save();
+
+            //Schedule a notification email to let the subscriber know that he has subscribed
+            //TODO
+
+        } catch (\Exception $e) {
+            // return eror
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+
+        //Return OK
+        return response()->json(['success' => "You have successfully subscribed your email - {$validated['email']} for the bitcoin tracker"], 200);
+    }
+
+    
+    public function unsubscribe(Request $request) {
+        try {
+            //Validate params
+            $validated = $request->validate([
+                'email' => 'bail|required||email:rfc,dns|exists:subscribers'
+            ]);
+
+            //forceDelete from DB the unsubscribed users
+            Subscriber::where('email', $validated['email'])->delete();
+
+            //Schedule a notification to let the subscriber know that he is no longer a subscriber
+            //TODO
+        } catch (\Exception $e) {
+            // return error
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
+
+        //Return OK
+        return response()->json(['success' => "You have successfully unsubscribed your email - {$validated['email']} from the bitcoin tracker"], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\SubscriberController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::controller(SubscriberController::class)->group(function () {
+    Route::get('/subscribe', 'subscribe')->name('subscribe'); //use get to let users subscribe even from the browser
+    Route::get('/unsubscribe', 'unsubscribe')->name('unsubscribe'); //using get to let users unsubscribe by visiting a link
+});
